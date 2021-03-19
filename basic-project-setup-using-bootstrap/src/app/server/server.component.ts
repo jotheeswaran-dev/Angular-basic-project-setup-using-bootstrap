d8af54e27dd421996adb93580fce183e35d6dd31
jotheeswaran-dev <79405572+jotheeswaran-dev@users.noreply.github.com>
2021-03-19 09:20:15 +0530

    Created new component 'Server' and added to app.module.ts to bundleup

---
+++ basic-project-setup-using-bootstrap/src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+// import decorators
+import { Component } from '@angular/core';
+
+// decorators
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+})
+
+// class defined - typescript 
+export class ServerComponent {
+
+}
