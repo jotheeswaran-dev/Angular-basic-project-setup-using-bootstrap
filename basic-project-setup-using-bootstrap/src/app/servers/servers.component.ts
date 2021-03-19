64819d7d80295db9fee9f4b1f6808ea355fd0c18
jotheeswaran-dev <79405572+jotheeswaran-dev@users.noreply.github.com>
2021-03-19 09:33:01 +0530

    Created component using CLI command

---
+++ basic-project-setup-using-bootstrap/src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+// Created this ServersComponent using CLI command 'ng generate component <name>' or 'ng g c <name>'
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
